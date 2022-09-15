aef1fffa26a731c0e9ffc49897362a3bea11d9ef
Nipa <akternipa941@gmail.com>
2022-09-15 14:51:35 +0600

    setup backend

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/user', userRouters);
+
+app.get('/', (req, res) => {
+    res.send('ACC Assignment-1');
+});
+
+
+app.listen(port, () => {
+    console.log(`ACC assignment listening on port ${port}`);
+});
+

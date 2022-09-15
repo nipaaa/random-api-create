b4cc954b8362d281017c28e81f20a432d94f5262
Nipa <akternipa941@gmail.com>
2022-09-15 14:55:47 +0600

    all user get api

--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -11,3 +11,11 @@ getUsers((data) =>{
 )
 
 const router = express.Router();
+
+router.get('/all', (req, res) =>{
+  const {limit} = req.query;
+  res.status(200).send({
+    status: 'Success',
+    data:users.slice(0, limit)
+  });
+});
